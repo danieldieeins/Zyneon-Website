ea4742ecbce9d90c61e853929fa430ba671f283d
nerotvlive <daniel.niesmann@outlook.com>
2024-03-12 13:35:29 +0100

    hopefully fixed unix theming issues

--- home/assets/js/shared.js
+++ home/assets/js/shared.js
@@ -1,3 +1,14 @@
+let menu = false;
 function toggleMenu() {
-    document.getElementById("links").classList.toggle("active");
+    if(menu) {
+        document.getElementById("links").classList.remove("active");
+        document.getElementById("toggle").classList.remove("bx-x");
+        document.getElementById("toggle").classList.add("bx-menu");
+        menu = false;
+    } else {
+        document.getElementById("links").classList.add("active");
+        document.getElementById("toggle").classList.remove("bx-menu");
+        document.getElementById("toggle").classList.add("bx-x");
+        menu = true;
+    }
 }
