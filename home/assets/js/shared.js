0f6a2f9ee87d9c38c0fe97bdcc3d76f9060190c3
nerotvlive <daniel.niesmann@outlook.com>
2024-03-14 10:55:57 +0100

    Fix menu theming issues and changes responsive behavior

--- home/assets/js/shared.js
+++ home/assets/js/shared.js
@@ -2,13 +2,13 @@ let menu = false;
 function toggleMenu() {
     if(menu) {
         document.getElementById("links").classList.remove("active");
-        document.getElementById("toggle").classList.remove("bx-x");
-        document.getElementById("toggle").classList.add("bx-menu");
+        document.getElementById("toggle-on").style.display = "none";
+        document.getElementById("toggle").style.display = "inherit";
         menu = false;
     } else {
         document.getElementById("links").classList.add("active");
-        document.getElementById("toggle").classList.remove("bx-menu");
-        document.getElementById("toggle").classList.add("bx-x");
+        document.getElementById("toggle-on").style.display = "inherit";
+        document.getElementById("toggle").style.display = "none";
         menu = true;
     }
 }
