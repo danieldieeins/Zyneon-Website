b0b19599e440f7e789047e80d78f43e823790798
nerotvlive <daniel.niesmann@outlook.com>
2024-03-14 11:03:02 +0100

    Close menu automatically when window width>768

--- home/assets/js/shared.js
+++ home/assets/js/shared.js
@@ -1,14 +1,30 @@
 let menu = false;
 function toggleMenu() {
     if(menu) {
-        document.getElementById("links").classList.remove("active");
-        document.getElementById("toggle-on").style.display = "none";
-        document.getElementById("toggle").style.display = "inherit";
-        menu = false;
+        closeMenu();
     } else {
-        document.getElementById("links").classList.add("active");
-        document.getElementById("toggle-on").style.display = "inherit";
-        document.getElementById("toggle").style.display = "none";
-        menu = true;
+        openMenu();
     }
 }
+
+function openMenu() {
+    menu = true;
+    document.getElementById("links").classList.add("active");
+    document.getElementById("toggle-on").style.display = "inherit";
+    document.getElementById("toggle").style.display = "none";
+}
+
+function closeMenu() {
+    menu = false;
+    document.getElementById("links").classList.remove("active");
+    document.getElementById("toggle-on").style.display = "none";
+    document.getElementById("toggle").style.display = "inherit";
+}
+
+function onResize() {
+    if(window.innerWidth>768) {
+        closeMenu();
+    }
+}
+
+window.onresize = onResize;
