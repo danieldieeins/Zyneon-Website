47f11daa537e1d407d9fdd87582eeb75f9710a0a
nerotvlive <daniel.niesmann@outlook.com>
2024-03-19 00:57:47 +0100

    Add background

--- home/assets/js/shared.js
+++ home/assets/js/shared.js
@@ -25,6 +25,26 @@ function onResize() {
     if(window.innerWidth>768) {
         closeMenu();
     }
+    resizeTitlebar();
+}
+
+function resizeTitlebar() {
+    const title= document.getElementById("top");
+
+    if(window.innerWidth>768) {
+        const p = window.innerWidth / 768 * 2;
+        let i = 2.25 * p;
+
+        if(i>25) {
+            i=25;
+        }
+
+        title.style.marginRight = i+"%";
+        title.style.marginLeft = i+"%";
+    } else {
+        title.style.marginRight = "5%";
+        title.style.marginLeft = "5%";
+    }
 }
 
 window.onresize = onResize;
